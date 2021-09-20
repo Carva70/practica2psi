fd35f12b5a6b80e5dacf6d42b7f6026728fd36a5
Carva70 <nachocargar@gmail.com>
2021-09-20 10:55:17 +0200

    cominezo 3

--- django_projects/locallibrary/catalog/models.py
+++ django_projects/locallibrary/catalog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+
+    # Fields
+    name = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField()
+    
+    ...
+
+    # Metadata
+    class Meta:
+        ordering = ['-my_field_name']
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.my_field_name
+
+
+class Books(models.Model):
+
+    # Fields
+    author = models.ForeignKey(
+        'Author',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField()
+    
+    ...
+
+    # Metadata
+    class Meta:
+        ordering = ['-my_field_name']
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.my_field_name
+
