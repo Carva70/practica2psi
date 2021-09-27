391966aff431eabc29cee7e3648f927a6ccbe8ef
Carva70 <nachocargar@gmail.com>
2021-09-27 10:50:45 +0200

    Co-authored-by: David Almeda Garcia <david.almeda@estudiante.uam.es>

--- django_projects/locallibrary/catalog/models.py
+++ django_projects/locallibrary/catalog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.fields.related import ManyToManyField
+from django.db.models.fields.reverse_related import ManyToOneRel
+import uuid # Required for unique book instances
 
 
 class Author(models.Model):
@@ -12,41 +15,100 @@ class Author(models.Model):
 
     # Metadata
     class Meta:
-        ordering = ['-my_field_name']
+        ordering = ['name']
 
     # Methods
     def get_absolute_url(self):
         """Returns the url to access a particular instance of MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
+        return self.reverse('model-detail-view', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.my_field_name
+        return self.name + self.date_of_birth + self.date_of_death
 
 
-class Books(models.Model):
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
-    # Fields
-    author = models.ForeignKey(
-        'Author',
-        on_delete=models.CASCADE,
+
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class Language(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.PROTECT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
     )
-    name = models.CharField(max_length=20)
-    date_of_birth = models.DateField()
-    date_of_death = models.DateField()
-    
-    ...
 
-    # Metadata
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
     class Meta:
-        ordering = ['-my_field_name']
-
-    # Methods
-    def get_absolute_url(self):
-        """Returns the url to access a particular instance of MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
+        ordering = ['due_back']
 
     def __str__(self):
-        """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.my_field_name
-
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
