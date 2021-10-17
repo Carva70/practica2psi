30af1e903a313695f413a4cc4986ef00165e159a
Carva70 <nachocargar@gmail.com>
2021-10-17 13:12:07 +0200

    heroku5

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -156,6 +156,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #django_heroku.settings(locals())
